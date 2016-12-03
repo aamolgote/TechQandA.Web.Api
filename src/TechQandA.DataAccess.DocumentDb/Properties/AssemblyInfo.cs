7f9d9c556bff39ed81783a3107309963b637c20f
Amol Gote <aamolgote@hotmail.com>
2016-12-03 16:34:42 -0500

    Initial Project Structure

---
+++ src/TechQandA.DataAccess.DocumentDb/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TechQandA.DataAccess.DocumentDb")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a12fa7f4-1bc3-41ad-a5ac-347d91a1b1f7")]
